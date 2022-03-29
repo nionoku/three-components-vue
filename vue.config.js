c9ec992b917866aec1702b7aac5edefa8594c6b5
nionoku <nionoku@gmail.com>
2022-03-29 16:07:47 +0300

    Update config

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,11 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  productionSourceMap: false,
+  configureWebpack: {
+    externals: {
+      three: 'three',
+      vue: 'vue',
+    },
+  },
 });
